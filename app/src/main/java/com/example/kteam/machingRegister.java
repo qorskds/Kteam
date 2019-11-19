d579dcf218c19e357bb5a51d54dbde670655c8b7
qorskds <43751150+qorskds@users.noreply.github.com>
2019-11-19 22:04:19 +0900

    20191192204

---
+++ app/src/main/java/com/example/kteam/machingRegister.java
@@ -0,0 +1,14 @@
+package com.example.kteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class machingRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maching_register);
+    }
+}
